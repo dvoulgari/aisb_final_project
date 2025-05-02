f08db5f88504de7dc1a3d53aa10ce297789be8b1
vex-me-not <nickcharisis182@gmail.com>
2025-05-03 01:06:39 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AISB_Final_Project
 Final Project for DSIT's course "Algorithms in Structural Bioinformatics"
+# Team members
+<li>Voulgari Despoina</li>
+<li>Charisis Nikos</li>
---
+++ src/functions.py
@@ -0,0 +1,9 @@
+
+
+
+# Compute the Euclidean distance between two points.
+def euclidean_distance(x, y):
+    """
+    The euclidean distance between two scalars is their absolute value.
+    """
+    return abs(x - y)
