f45077a5a32d5a2683dbbe1b408dff21b49abb4f
vex-me-not <nickcharisis182@gmail.com>
2025-05-12 22:10:13 +0300

    very untested version of dtw

--- src/functions.py
+++
@@ -1,9 +0,0 @@
-
-
-
-# Compute the Euclidean distance between two points.
-def euclidean_distance(x, y):
-    """
-    The euclidean distance between two scalars is their absolute value.
-    """
-    return abs(x - y)
